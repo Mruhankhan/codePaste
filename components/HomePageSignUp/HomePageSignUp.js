55c87a7c1b15522322c99c59fedd9d85d7b6e11d
Muhammad Khan <s1942405@online.houstonisd.org>
2022-07-16 18:59:07 -0500

    Work

--- components/HomePageSignUp/HomePageSignUp.js
+++ components/HomePageSignUp/HomePageSignUp.js
@@ -11,25 +11,37 @@ export default function HomePageSignUp() {
     username:'',
     password:'',
   })
-  
-  const FirstNameChange = () =>{
-    
+
+  const firstNameChange = (e) =>{
+    newValues({...values, firstName: e.target.value })
   }
 
+ 
 
-
-
-
+  const lastNameChange = (e) =>{
+    newValues({...values, lastName: e.target.value })
+  }
+  const emailChange = (e) =>{
+    newValues({...values, email: e.target.value })
+  }
+  
+  const usernameChange = (e) =>{
+    newValues({...values, username: e.target.value })
+  }
+  const passwordChange = (e) =>{
+    newValues({...values, password: e.target.value })
+  }
+  
 
 
   return (
     <>
         <form action=''>
-        <input  type='text'  value={values.firstName}  placeholder='First Name' id='Name'/>
-        <input type='text' value={values.lastName}  placeholder='Last Name'/>
-        <input type='email' value={values.email}  placeholder='Enter Your Email'/>
-        <input type='text' value={values.username} placeholder='Enter Username'/>
-        <input type='password' value={values.password} placeholder='Enter An Password'></input>
+        <input  type='text'  value={values.firstName} onChange={firstNameChange} placeholder='First Name' id='Name'/>
+        <input type='text' value={values.lastName}  onChange={lastNameChange}  placeholder='Last Name'/>
+        <input type='email' value={values.email}        onChange={emailChange}  placeholder='Enter Your Email'/>
+        <input type='text' value={values.username}      onChange={usernameChange}  placeholder='Enter Username'/>
+        <input type='password' value={values.password}  onChange={passwordChange}  placeholder='Enter An Password'></input>
         <button type='submit'>Submit</button>
         </form>
         <button >Login Page</button>
