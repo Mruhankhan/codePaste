c2ee2537b94a6060d8d815181b54b99303dfab2e
Muhammad Khan <s1942405@online.houstonisd.org>
2022-07-17 10:22:22 -0500

    Work

--- components/HomePageSignUp/HomePageSignUp.js
+++ components/HomePageSignUp/HomePageSignUp.js
@@ -11,6 +11,9 @@ export default function HomePageSignUp() {
     username:'',
     password:'',
   })
+  
+  const [submitted, setSubmitted ] = useState(false)
+  const [formVaild, setFormValid] = useState(false)
 
   const firstNameChange = (e) =>{
     newValues({...values, firstName: e.target.value })
@@ -32,16 +35,35 @@ export default function HomePageSignUp() {
     newValues({...values, password: e.target.value })
   }
   
-
+  const sumbitForm = (e) =>{
+    e.preventDefault()
+    if (values.firstName && values.lastName && values.email && values.username && values.password) {
+      setFormValid(true)
+    }
+      setSubmitted(true)
+  
+  }
 
   return (
     <>
-        <form action=''>
+        <form  onSubmit={sumbitForm}>
+         {submitted && formVaild ? <div>Thanks!</div> : null} 
         <input  type='text'  value={values.firstName} onChange={firstNameChange} placeholder='First Name' id='Name'/>
+       
+       {submitted && !values.firstName ? <div>Please Enter Your First Name</div> : null } 
+       
         <input type='text' value={values.lastName}  onChange={lastNameChange}  placeholder='Last Name'/>
+       {submitted && !values.lastName ? <div>Please Enter Your last Name</div> : null } 
+
         <input type='email' value={values.email}        onChange={emailChange}  placeholder='Enter Your Email'/>
-        <input type='text' value={values.username}      onChange={usernameChange}  placeholder='Enter Username'/>
-        <input type='password' value={values.password}  onChange={passwordChange}  placeholder='Enter An Password'></input>
+        {submitted && !values.email ? <div>Please Enter Your Email</div> : null}
+
+        <input type='text' value={values.username}      onChange={usernameChange}  placeholder='MAke an Username'/>
+       {submitted && !values.username ? <div>Please Make An Username</div> : null} 
+
+        <input type='password' value={values.password}  onChange={passwordChange}  placeholder='Make An Password'/>
+       {submitted && !values.password ? <div>Please Make An Password</div> : null} 
+
         <button type='submit'>Submit</button>
         </form>
         <button >Login Page</button>
